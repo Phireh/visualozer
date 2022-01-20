f2c48bcc3e757426dcfca50435a85bdbb0bea82f
Phireh <Phireh@fukurokuju.dev>
2022-01-20 14:47:05 +0100

    Add nuklear lib for GUI

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 CC=gcc
 CFLAGS=-Wall -Wextra -g -pedantic -std=c11
 LDFLAGS=-lpthread -lm
+LDFLAGS+=`pkg-config --libs glfw3 glew`
 SRCDIR=src
 
 
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S), Linux) # flags for Linux build
 	LDFLAGS+=-ldl
-endif
+endif ($(UNAME_S), Darwin) # flags for the Mac build
+	LDFLAGS+=-framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -L/usr/local/lib
+ifeq 
 
 visualozer: $(SRCDIR)/main.c
-	$(CC) $(CFLAGS) $(LDFLAGS) $(SRCDIR)/main.c -o visualozer
+	$(CC) $(SRCDIR)/main.c $(CFLAGS) -o visualozer $(LDFLAGS)
 
 clean:
 	rm visualozer
--- src/main.c
+++ src/main.c
@@ -18,6 +18,33 @@
 #pragma GCC diagnostic pop
 
 
+/* Nuklear / GLFW includes */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#define NK_KEYSTATE_BASED_INPUT
+#include "nuklear.h"
+#include "nuklear_glfw_gl3.h"
+
+
+
+// TODO: Make a nicer error callback for glfw
+static void error_callback(int e, const char *d)
+{
+    printf("Error %d: %s\n", e, d);
+}
+
+
+
 // TODO: This is placeholder code that just outputs the sound out of the decoder, for now
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
@@ -90,7 +117,7 @@ int main(int argc, char *argv[])
     /* TODO: this is placeholder code. Miniaudio objects have manual memory management, so we'd (probably) want to make these objects
        globals or heap-allocate them, depending on their size. */
 
-    ma_result result;
+    //ma_result result;
     ma_decoder decoder;
     ma_device_config device_conf;
     ma_device device;
@@ -124,10 +151,125 @@ int main(int argc, char *argv[])
         return -4;
     }
 
+
     printf("Playing %s, press enter to quit...\n", input_file_path);
-    getchar();
+
+    /* Platform vars */
+    struct nk_glfw glfw = {0};
+    static GLFWwindow *win;
+    int width = 0, height = 0;
+    struct nk_context *ctx;
+    struct nk_colorf bg;
+
+    /* GLFW initialization */
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialized glfw\n");
+        goto cleanup_and_exit;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    // TODO: remove magic numbers
+    win = glfwCreateWindow(1024, 720, "Visualozer", NULL, NULL);
+    glfwMakeContextCurrent(win);
+    glfwGetWindowSize(win, &width, &height);
 
 
+    /* Get openGL context */
+
+    glViewport(0, 0, width, height);
+    glewExperimental = 1;
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to setup GLEW\n");
+        goto cleanup_and_exit;
+    }
+
+    // TODO: Move this
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
+    
+    ctx = nk_glfw3_init(&glfw, win, NK_GLFW3_INSTALL_CALLBACKS);
+
+    {
+        struct nk_font_atlas *atlas;
+        nk_glfw3_font_stash_begin(&glfw, &atlas);
+        nk_glfw3_font_stash_end(&glfw);
+    }
+
+
+    bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
+
+    while (!glfwWindowShouldClose(win))
+    {
+        /* Input */
+        glfwPollEvents();
+
+
+        /* GUI */
+        nk_glfw3_new_frame(&glfw);
+        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
+                     NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+                     NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        {
+            enum {EASY, HARD};
+            static int op = EASY;
+            static int property = 20;
+            nk_layout_row_static(ctx, 30, 80, 1);
+            if (nk_button_label(ctx, "button"))
+                fprintf(stdout, "button pressed\n");
+
+            nk_layout_row_dynamic(ctx, 30, 2);
+            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+
+            nk_layout_row_dynamic(ctx, 25, 1);
+            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+            nk_layout_row_dynamic(ctx, 20, 1);
+            nk_label(ctx, "background:", NK_TEXT_LEFT);
+            nk_layout_row_dynamic(ctx, 25, 1);
+            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
+                nk_layout_row_dynamic(ctx, 120, 1);
+                bg = nk_color_picker(ctx, bg, NK_RGBA);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
+                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
+                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
+                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
+                nk_combo_end(ctx);
+            }
+        }
+        nk_end(ctx);
+
+
+
+        /* Draw */
+        glfwGetWindowSize(win, &width, &height);
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(bg.r, bg.g, bg.b, bg.a);
+        /* IMPORTANT: `nk_glfw_render` modifies some global OpenGL state
+         * with blending, scissor, face culling, depth test and viewport and
+         * defaults everything back into a default state.
+         * Make sure to either a.) save and restore or b.) reset your own state after
+         * rendering the UI. */
+        nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+        glfwSwapBuffers(win);
+        
+    }
+
+
+    
+cleanup_and_exit:
+    nk_glfw3_shutdown(&glfw);
+    glfwTerminate();
     ma_device_uninit(&device);
     ma_decoder_uninit(&decoder);
     return 0;
